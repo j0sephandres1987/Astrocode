c4495015da6da47c93a999345be5d5e4dbd66d0e
Koding <j0sephandres@j0sephandres.koding.io>
2016-02-20 11:54:28 -0600

    goright movement

--- js/core.js
+++ js/core.js
@@ -11,11 +11,25 @@ $(document).ready(function (){
   
   function create() {
     game.world.setBounds(0, 0, 800, 800);
+
     background = game.add.tileSprite(0, 0, 800, 600, "background");
     background.fixedToCamera=true;
-    astro = game.add.sprite(50, 430, 'astronaut',0); //Character sprites
+    astro = game.add.sprite(50, 430, 'astronaut', 0); //Character sprites
     astro.scale.setTo(0.8, 0.8);
     astro.anchor.setTo(0.5, 0.5);
+    astro.animations.add('right', ['6.png', '2.png', '6.png', '5.png', '6.png', '4.png'], 12, true);
+    //game.physics.enable(astro, true);
   }
+  
+  function goRight() {
+    astro.animations.play('right');
+    var movement = setInterval(function(){
+      astro.x += 2.2;
+    }, 10);
+  }
+  
+  $("#phaserContainer").click(function() {
+    goRight();
+  });
   
 });
